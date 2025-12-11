f3927f823e9214b693a8e6862a20be34842c177c
Kishan <kingbind852@gmail.com>
2025-12-11 23:57:00 +0530

    all btn added

--- app/src/main/java/com/example/ck_mapper/MainActivity.java
+++ app/src/main/java/com/example/ck_mapper/MainActivity.java
@@ -31,10 +31,33 @@ public class MainActivity extends AppCompatActivity {
 
         btnsList = new ArrayList<>();
 
+        //direction button
         btnsList.add(new Custom_btn_model(R.drawable.directional_btn,"Up",270f));
         btnsList.add(new Custom_btn_model(R.drawable.directional_btn,"Right",0f));
         btnsList.add(new Custom_btn_model(R.drawable.directional_btn,"Down",90f));
         btnsList.add(new Custom_btn_model(R.drawable.directional_btn,"Left",180f));
+
+        //face Button
+        btnsList.add(new Custom_btn_model(R.drawable.face_btn_y,"Y",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.face_btn_b,"B",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.face_btn_a,"A",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.face_btn_x,"X",0f));
+
+        //start and select
+        btnsList.add(new Custom_btn_model(R.drawable.btn_start,"start",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.btn_select,"select",0f));
+
+        //joysticks
+        btnsList.add(new Custom_btn_model(R.drawable.joystick,"J1",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.joystick,"J2",0f));
+
+        //Triggers
+        btnsList.add(new Custom_btn_model(R.drawable.btn_l1,"L1",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.btn_r1,"R1",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.btn_l1,"L2",0f));
+        btnsList.add(new Custom_btn_model(R.drawable.btn_l2,"R2",0f));
+
+
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
